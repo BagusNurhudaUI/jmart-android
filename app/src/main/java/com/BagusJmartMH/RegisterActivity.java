f679fc1adc80c3d5d2f0874a1a283843ff2d0214
BagusNurhudaUI <bagus.nurhuda@ui.ac.id>
2021-11-26 20:54:31 +0700

    TP modul 9 - Bagus Nurhuda

---
+++ app/src/main/java/com/BagusJmartMH/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.BagusJmartMH;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
